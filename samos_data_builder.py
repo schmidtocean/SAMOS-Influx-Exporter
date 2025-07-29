2089ae4f41894b75f02ab14bf5bc6bda69fe1604
Webb Pinner <webbpinner@gmail.com>
2025-07-29 11:22:31 -0300

    lint'd files and cleanup logic
    also restored ability to add cc recipients to emails

--- samos_data_builder.py
+++ samos_data_builder.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''
+"""
 FILE:           samos_data_builder.py
 
 DESCRIPTION:    This script builds samos data using data pulled from influxDB.
@@ -14,75 +14,87 @@ REVISION:   2022-10-27
 
 LICENSE INFO:   This code is licensed under GPLv3 license (see LICENSE.txt for
                 details). Copyright (C) Schmidt Ocean Institute 2022
-'''
+"""
 
-import sys
-import re
 import logging
+import re
+import sys
 from datetime import datetime, timedelta
-import pytz
-from urllib3.exceptions import NewConnectionError, ProtocolError
-from influxdb_client import InfluxDBClient, Dialect
-from influxdb_client.rest import ApiException
-
 from os.path import dirname, realpath
-sys.path.append(dirname(dirname(dirname(realpath(__file__)))))
 
-from settings import INFLUX_SERVER_URL, INFLUX_TOKEN, INFLUX_ORG, \
-                     INFLUX_BUCKET
-from samos_fields import SAMOS_FIELDS
+import pytz
+from influxdb_client import Dialect, InfluxDBClient
+from influxdb_client.rest import ApiException
+from urllib3.exceptions import (ConnectTimeoutError, NewConnectionError,
+                                ProtocolError)
+
+sys.path.append(dirname(realpath(__file__)))
+from samos_fields import SAMOS_FIELDS  # noqa: E402
+from settings import (INFLUX_BUCKET, INFLUX_ORG,  # noqa: E402
+                      INFLUX_SERVER_URL, INFLUX_TOKEN)
 
 # Example of SAMOS format
-# $SAMOS:001,CS:KAOU,YMD:20030907,HMS:000011,AT:17.40,BP:1010.27,WSP:5.6,WDP:354.4,TWP:5.4,TIP:278.3,WSS:6.7,WDS:350.5,TWS:6.6,TIS:274.4,LA:44.66956,LO:-130.35859,COG:149.5,SOG:0.9,GY:284.7,CS8:23
+# $SAMOS:001,CS:KAOU,YMD:20030907,HMS:000011,AT:17.40,BP:1010.27,WSP:5.6,WDP:354.4,TWP:5.4,TIP:278.3,WSS:6.7,WDS:350.5,TWS:6.6,TIS:274.4,LA:44.66956,LO:-130.35859,COG:149.5,SOG:0.9,GY:284.7,CS8:23  # noqa: E501
 
-class SAMOSDataBuilder():
-    '''
+
+class SAMOSDataBuilder:
+    """
     Class that handles the construction of an influxDB query and using the
     resulting data to build the SAMOS csv format.
-    '''
+    """
 
     def __init__(self, samos_data_config, influxdb_client=None):
 
-        self.influxdb_client = influxdb_client or InfluxDBClient(url=INFLUX_SERVER_URL,
-                                                                 token=INFLUX_TOKEN,
-                                                                 org=INFLUX_ORG
-                                                                )
+        self.influxdb_client = influxdb_client or InfluxDBClient(
+            url=INFLUX_SERVER_URL, token=INFLUX_TOKEN, org=INFLUX_ORG,
+            timeout=5_000  # in milliseconds (5 seconds)
+        )
         self._influxdb_client_api = self.influxdb_client.query_api()
 
-        self.callsign = samos_data_config['callsign']
-        self._query_measurements = samos_data_config['measurements']
-        self._query_fields = [v for _, v in samos_data_config['fields'].items()]
-        self._fields = samos_data_config['fields']
+        self.callsign = samos_data_config["callsign"]
+        self._query_measurements = samos_data_config["measurements"]
+        self._query_fields = [
+            v for _, v in samos_data_config["fields"].items()
+        ]
+        self._fields = samos_data_config["fields"]
 
         self._influx_query_result = None
         self.logger = logging.getLogger(__name__)
 
         for field in self._fields.keys():
-            if field[:2] not in SAMOS_FIELDS or re.search('^[A-Z]{2}[0-9]?', field) is None:
-                logging.warning('Field: %s is not a standard SAMOS field identifier', field)
+            if (
+                field[:2] not in SAMOS_FIELDS
+                or re.search("^[A-Z]{2}[0-9]?", field) is None
+            ):
+                logging.warning(
+                    "Field: %s is not a standard SAMOS field identifier", field
+                )
 
     @staticmethod
-    def _build_query_range(ts): # pylint: disable=invalid-name
-        '''
+    def _build_query_range(ts):  # pylint: disable=invalid-name
+        """
         Builds the temporal range for the influxDB query based on the provided
         timestamp (ts).
-        '''
+        """
 
         try:
             start_ts = ts.replace(tzinfo=pytz.utc)
             stop_ts = start_ts + timedelta(days=1)
-            return f'start: {start_ts.strftime("%Y-%m-%dT00:00:00.000Z")}, stop: {stop_ts.strftime("%Y-%m-%dT00:00:00.000Z")}'
+            return (
+                f'start: {start_ts.strftime("%Y-%m-%dT00:00:00.000Z")}, '
+                f'stop: {stop_ts.strftime("%Y-%m-%dT00:00:00.000Z")}'
+            )
 
         except Exception as err:
             logging.debug(str(err))
 
         return None
 
-    def _build_query(self, ts=None): # pylint: disable=invalid-name
-        '''
+    def _build_query(self, ts=None):  # pylint: disable=invalid-name
+        """
         Builds the complete influxDB query using the provided timestamp (ts)
         and the query_measurements and query_fields class properties.
-        '''
+        """
 
         if ts is None:
             ts = datetime.utcnow()
@@ -91,68 +103,93 @@ class SAMOSDataBuilder():
 
         try:
             query = f'from(bucket: "{INFLUX_BUCKET}")\n'
-            query += f'|> range({query_range})\n'
-            query += '|> filter(fn: (r) => {})\n'.format(' or '.join([ 'r["_measurement"] == "{}"'.format(q_measurement) for q_measurement in self._query_measurements]))
-            query += '|> filter(fn: (r) => {})\n'.format(' or '.join([ 'r["_field"] == "{}"'.format(q_field) for q_field in self._query_fields]))
+            query += f"|> range({query_range})\n"
+            query += "|> filter(fn: (r) => {})\n".format(
+                " or ".join(
+                    [
+                        'r["_measurement"] == "{}"'.format(q_measurement)
+                        for q_measurement in self._query_measurements
+                    ]
+                )
+            )
+            query += "|> filter(fn: (r) => {})\n".format(
+                " or ".join(
+                    [
+                        'r["_field"] == "{}"'.format(q_field)
+                        for q_field in self._query_fields
+                    ]
+                )
+            )
             query += '|> keep(columns: ["_time", "_field", "_value"])'
-            query += '|> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")'
+            query += '|> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")'  # noqa: E501
         except Exception as err:
-            logging.error('Error building query string')
-            logging.error(' - Range: %s', query_range)
-            logging.error(' - Measurements: %s', self._query_measurements)
-            logging.error(' - Fields: %s', self._query_fields)
+            logging.error("Error building query string")
+            logging.error(" - Range: %s", query_range)
+            logging.error(" - Measurements: %s", self._query_measurements)
+            logging.error(" - Fields: %s", self._query_fields)
             raise err
 
-        logging.debug("Query: %s", query)
+        logging.debug("Query:\n  %s", query
+                      .replace('\n', '\n  ')
+                      .replace(' or ', '\n     or '))
         return query
 
-
-    def build_samos_csv(self, ts): # pylint: disable=too-many-branches
-        '''
+    def build_samos_csv(self, ts):  # pylint: disable=too-many-branches
+        """
         Build the SAMOS csv output for the given timestamp (ts)
-        '''
+        """
 
         if self._influx_query_result is None:
-            self.retrieve_samos_data(ts)
+            try:
+                self.retrieve_samos_data(ts)
+            except Exception:
+                sys.exit(1)
 
         if self._influx_query_result is None:
-            logging.info("Did not find any data for %s", ts.strftime("%Y-%m-%d"))
+            logging.info("Did not find any data for %s",
+                         ts.strftime("%Y-%m-%d"))
             yield None
 
-        csv_result = self._influxdb_client_api.query_csv(self._build_query(ts),
-                                                    dialect=Dialect(header=True,
-                                                        delimiter=",",
-                                                        comment_prefix="#",
-                                                        annotations=[],
-                                                        date_time_format="RFC3339")
-                                                    )
+        csv_result = self._influxdb_client_api.query_csv(
+            self._build_query(ts),
+            dialect=Dialect(
+                header=True,
+                delimiter=",",
+                comment_prefix="#",
+                annotations=[],
+                date_time_format="RFC3339",
+            ),
+        )
 
         try:
-            header = csv_result.__next__()
-        except:
+            header = next(csv_result)
+        except StopIteration:
             pass
-    
+
+        errors = []
         for _, csv_line in enumerate(csv_result):
             if not len(csv_line) == 0:
                 columns = [
-                    f'$SAMOS:001',
-                    f'CS:{self.callsign}',
-                    f'YMD:{csv_line[header.index("_time")][:10].replace("-","")}',
-                    f'HMS:{csv_line[header.index("_time")][11:19].replace(":","")}'
+                    "$SAMOS:001",
+                    f"CS:{self.callsign}",
+                    f'YMD:{csv_line[header.index("_time")][:10].replace("-","")}',  # noqa: E501
+                    f'HMS:{csv_line[header.index("_time")][11:19].replace(":","")}',  # noqa: E501
                 ]
                 for key, val in self._fields.items():
                     try:
                         columns.append(f"{key}:{csv_line[header.index(val)]}")
                     except ValueError as err:
-                        logging.warning(str(err))
+                        if str(err) not in errors:
+                            errors.append(str(err))
+                            logging.warning(str(err))
                         columns.append(f"{key}:NaN")
 
-                yield ",".join(columns) + '\n'
+                yield ",".join(columns) + "\n"
 
     def retrieve_samos_data(self, ts=None):
-        '''
+        """
         Retrive the SAMOS data from InfluxDB.
-        '''
+        """
 
         logging.info("Retrieving SAMOS Data")
 
@@ -160,13 +197,26 @@ class SAMOSDataBuilder():
 
         # run the query against the influxDB
         try:
-            self._influx_query_result = self._influxdb_client_api.query(query=query)
+            self._influx_query_result = self._influxdb_client_api.query(
+                query=query)
 
-        except NewConnectionError:
-            logging.error("InfluxDB connection error, verify URL: %s", INFLUX_SERVER_URL)
+        except NewConnectionError as err:
+            logging.error(
+                "New connection error, verify URL: %s", INFLUX_SERVER_URL
+            )
+            raise err
 
-        except ProtocolError:
-            logging.error("InfluxDB connection error, verify URL: %s", INFLUX_SERVER_URL)
+        except ProtocolError as err:
+            logging.error(
+                "Connection protocol error, verify URL: %s", INFLUX_SERVER_URL
+            )
+            raise err
+
+        except ConnectTimeoutError as err:
+            logging.error(
+                "Connection timeout error, verify URL: %s", INFLUX_SERVER_URL
+            )
+            raise err
 
         except ApiException as err:
             _, value, _ = sys.exc_info()
@@ -174,42 +224,44 @@ class SAMOSDataBuilder():
             if str(value).startswith("(400)"):
                 logging.error("InfluxDB API error, verify org: %s", INFLUX_ORG)
             elif str(value).startswith("(401)"):
-                logging.error("InfluxDB API error, verify token: %s", INFLUX_TOKEN)
+                logging.error("InfluxDB API error, verify token: %s",
+                              INFLUX_TOKEN)
             elif str(value).startswith("(404)"):
-                logging.error("InfluxDB API error, verify bucket: %s", INFLUX_BUCKET)
+                logging.error("InfluxDB API error, verify bucket: %s",
+                              INFLUX_BUCKET)
             else:
-                raise err
+                logging.error("Unknown API error")
+            raise err
 
         except Exception as err:
             logging.error("Error with query:")
-            logging.error(query.replace("|>", '\n'))
-            logging.error(str(err))
-
+            logging.error(query)
+            raise err
 
     @property
     def samos_fields(self):
-        '''
+        """
         Getter method for the samos_fields property
-        '''
+        """
         return self._fields
 
     @property
     def measurements(self):
-        '''
+        """
         Getter method for the _query_measurements property
-        '''
+        """
         return self._query_measurements
 
     @property
     def fields(self):
-        '''
+        """
         Getter method for the _query_fields property
-        '''
+        """
         return self._query_fields
 
     @property
     def query_result(self):
-        '''
+        """
         Getter method for the _influx_query_result property
-        '''
+        """
         return self._influx_query_result
--- samos_exporter.py
+++ samos_exporter.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-'''
+"""
 FILE:           samos_exporter.py
 
-DESCRIPTION:    This process export data from InfluxDB in the SAMOS data format.
+DESCRIPTION:    This process export data from InfluxDB in the SAMOS data
+                format.
 
 BUGS:
 NOTES:
@@ -14,89 +15,113 @@ REVISION:   2024-08-14
 
 LICENSE INFO:   This code is licensed under GPLv3 license (see LICENSE.txt for
                 details). Copyright (C) Schmidt Ocean Institute 2024
-'''
+"""
 
-import os
-import sys
+import itertools
 import json
 import logging
+import os
+import shutil
+import sys
 import tempfile
-import itertools
-import yaml
 from datetime import datetime, timedelta
-
 from os.path import dirname, realpath
-sys.path.append(dirname(dirname(realpath(__file__))))
 
-from samos_data_builder import SAMOSDataBuilder
-from settings import MAILER_SUBJECT, MAILER_FROM, MAILER_TO, MAILER_CC, \
-                     MAILER_TEXT, EMAIL_FN_PREFIX, FN_PREFIX, DEST_DIR, \
-                     INLINE_CONFIG
+import yaml
+
+sys.path.append(dirname(realpath(__file__)))
+from gmailer_oauth import GMailer  # noqa: E402
+from samos_data_builder import SAMOSDataBuilder  # noqa: E402
+from settings import (DEST_DIR, EMAIL_FN_PREFIX, FN_PREFIX,  # noqa: E402
+                      INLINE_CONFIG, MAILER_CC, MAILER_FROM, MAILER_SUBJECT,
+                      MAILER_TEXT, MAILER_TO)
 
 
-import logging
-from gmailer_oauth import gMailer
-
-logging.basicConfig(level=logging.INFO)
-
-def send_samos_email(dt:datetime, samos_data_fp):
-    mailer = gMailer(
-        token_file='token.json',
-        client_secret_file='client_secret.json',
+def send_samos_email(dt: datetime, attachment_path: str):
+    mailer = GMailer(
+        token_file="token.json",
+        client_secret_file="client_secret.json",
         sender=MAILER_FROM,
         recipient=MAILER_TO,
     )
     try:
-        mailer.send_email(f'{MAILER_SUBJECT} - {dt.strftime("%Y-%m-%d")}', MAILER_TEXT.replace('<date>', dt.strftime("%Y-%m-%d")), [f'{DEST_DIR}/{EMAIL_FN_PREFIX}_{dt.strftime("%Y-%m-%d")}.csv'])
+        mailer.send_email(
+            subject=f'{MAILER_SUBJECT} - {dt.strftime("%Y-%m-%d")}',
+            body=MAILER_TEXT.replace("<date>", dt.strftime("%Y-%m-%d")),
+            attachments=[(attachment_path)],
+            cc=MAILER_CC
+        )
 
-    except Exception as err:
-        logging.error("Problem emailing SAMOS data")
-        logging.debug(str(err))
+    except Exception:
+        logging.exception("Problem emailing SAMOS data")
+
 
 def save_to_file(dt: datetime, samos_data_fp):
-    '''
+    """
     Save exported SAMOS data to file
-    '''
+    """
 
     try:
-        samos_filename = os.path.join(DEST_DIR, f'{FN_PREFIX}_{dt.strftime("%Y-%m-%d")}.csv')
+        samos_filename = os.path.join(
+            DEST_DIR, f'{FN_PREFIX}_{dt.strftime("%Y-%m-%d")}.csv'
+        )
 
-        with open(samos_filename, 'w') as fp:
+        with open(samos_filename, "w") as fp:
             fp.write(samos_data_fp.read())
 
-    except Exception as err:
-        logging.error("Problem saving SAMOS data to file")
-        logging.debug(str(err))
+    except Exception:
+        logging.exception("Problem saving SAMOS data to file")
+
 
 # -------------------------------------------------------------------------------------
 # The main function of the utility
 # -------------------------------------------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     import argparse
 
-    parser = argparse.ArgumentParser(description='SAMOS Data Exporter')
+    parser = argparse.ArgumentParser(description="SAMOS Data Exporter")
 
-    parser.add_argument('-q', '--quiet', action='store_true',
-                        help='Reduce output verbosity')
+    parser.add_argument(
+        "-q", "--quiet", action="store_true", help="Reduce output verbosity"
+    )
 
-    parser.add_argument('-v', '--verbosity', dest='verbosity',
-                        default=1, action='count',
-                        help='Increase output verbosity')
+    parser.add_argument(
+        "-v",
+        "--verbosity",
+        dest="verbosity",
+        default=1,
+        action="count",
+        help="Increase output verbosity",
+    )
 
-    parser.add_argument('-d', '--date', default=(datetime.utcnow() - timedelta(days=1)).strftime('%Y-%m-%d'),
-                        type=lambda s: datetime.strptime(s, '%Y-%m-%d'),
-                        help='Specify date of data to export (YYYY-mm-dd)')
+    parser.add_argument(
+        "-d",
+        "--date",
+        default=(datetime.utcnow() - timedelta(days=1)).strftime("%Y-%m-%d"),
+        type=lambda s: datetime.strptime(s, "%Y-%m-%d"),
+        help="Specify date of data to export (YYYY-mm-dd)",
+    )
 
-    parser.add_argument('-e', '--email', default=False,
-                        action='store_true',
-                        help='Send email containing exported data to SAMOS')
+    parser.add_argument(
+        "-e",
+        "--email",
+        default=False,
+        action="store_true",
+        help="Send email containing exported data to SAMOS",
+    )
 
-    parser.add_argument('-s', '--save', default=False,
-                        action='store_true',
-                        help='Save exported data to file')
+    parser.add_argument(
+        "-s",
+        "--save",
+        default=False,
+        action="store_true",
+        help="Save exported data to file",
+    )
 
-    parser.add_argument('-f', '--config_file', help='Used the specifed configuration file')
+    parser.add_argument(
+        "-f", "--config_file", help="Used the specifed configuration file"
+    )
 
     parsed_args = parser.parse_args()
 
@@ -104,10 +129,15 @@ if __name__ == '__main__':
     # Set up logging before we do any other argument parsing (so that we
     # can log problems with argument parsing).
 
-    LOGGING_FORMAT = '%(asctime)-15s %(levelname)s - %(message)s'
+    LOGGING_FORMAT = "%(asctime)-15s %(levelname)s - %(message)s"
     logging.basicConfig(format=LOGGING_FORMAT)
 
-    LOG_LEVELS = {0: logging.ERROR, 1: logging.WARNING, 2: logging.INFO, 3: logging.DEBUG}
+    LOG_LEVELS = {
+        0: logging.ERROR,
+        1: logging.WARNING,
+        2: logging.INFO,
+        3: logging.DEBUG,
+    }
 
     if parsed_args.quiet:
         logging.getLogger().setLevel(LOG_LEVELS[0])
@@ -115,20 +145,20 @@ if __name__ == '__main__':
         parsed_args.verbosity = min(parsed_args.verbosity, max(LOG_LEVELS))
         logging.getLogger().setLevel(LOG_LEVELS[parsed_args.verbosity])
 
-    samos_data_config = None # pylint: disable=invalid-name
+    samos_data_config = None  # pylint: disable=invalid-name
 
     if parsed_args.config_file:
         try:
-            with open(parsed_args.config_file, "r", encoding='utf-8') as file:
+            with open(parsed_args.config_file, "r", encoding="utf-8") as file:
                 samos_data_config = yaml.safe_load(file)
         except yaml.parser.ParserError:
-            logging.error("Invalid YAML syntax")
+            logging.exception("Invalid YAML syntax")
             sys.exit(1)
     else:
         try:
             samos_data_config = yaml.safe_load(INLINE_CONFIG)
         except yaml.parser.ParserError:
-            logging.error("Invalid YAML syntax")
+            logging.exception("Invalid YAML syntax")
             sys.exit(1)
 
     logging.debug(json.dumps(samos_data_config, indent=2))
@@ -144,29 +174,31 @@ if __name__ == '__main__':
         logging.info("No data found, Quitting")
         sys.exit(0)
 
-    fd, path = tempfile.mkstemp()
+    if not (parsed_args.email or parsed_args.save):
+        for line in itertools.chain([peek], output):
+            print(line)
+        sys.exit(0)
+
+    tmp_dir = tempfile.gettempdir()
+    samos_fn = f"{EMAIL_FN_PREFIX}_{parsed_args.date.strftime('%Y-%m-%d')}.csv"
+    samos_fp = os.path.join(tmp_dir, samos_fn)
 
     try:
-        with os.fdopen(fd, 'r+') as fp:
+        with open(samos_fp, "w+") as f:
             for line in itertools.chain([peek], output):
-                fp.write(line)
+                f.write(line)
+
+            f.seek(0)
 
             # If the data should be emailed to SAMOS
             if parsed_args.email:
                 logging.info("Emailing exported data to: %s", MAILER_TO)
-                fp.seek(0)
-                send_samos_email(parsed_args.date, fp)
+                send_samos_email(parsed_args.date, samos_fp)
 
-            # If the data should be emailed to SAMOS
             if parsed_args.save:
-                logging.info("Saving exported data to: %s", os.path.join(DEST_DIR, f'{FN_PREFIX}_{parsed_args.date.strftime("%Y-%m-%d")}.csv'))
-                fp.seek(0)
-                save_to_file(parsed_args.date, fp)
-
-            # If the data was not emailed or saved to file, send to stdout
-            if not (parsed_args.email or parsed_args.save):
-                fp.seek(0)
-                print(fp.read())
+                dest_filename = os.path.join(DEST_DIR, samos_fn)
+                shutil.copy(samos_fp, dest_filename)
+                logging.info("Saved exported data to: %s", dest_filename)
 
     finally:
-        os.remove(path)
+        os.remove(samos_fp)
